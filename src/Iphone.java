907a365a0c20beff4dca68491929bca53504f19f
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2024-05-15 18:38:19 -0300

    desafio feito

---
+++ src/Iphone.java
@@ -0,0 +1,46 @@
+public class Iphone  implements AparelhoTelefonico, NavagarInternet, ReprodutorMusical{
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina() {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+}
---
+++ src/NavagarInternet.java
@@ -0,0 +1,5 @@
+public interface NavagarInternet {
+    void exibirPagina();
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
